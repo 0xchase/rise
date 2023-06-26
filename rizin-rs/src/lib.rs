715471a04751db046311d175f766d392c1f9da43
Heather <hbitsoi@tacnetsol.com>
2023-06-26 16:56:24 -0400

    udpates

--- rizin-rs/src/lib.rs
+++ rizin-rs/src/lib.rs
@@ -3,7 +3,6 @@ use rizin_sys::*;
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
-
 struct RzCore {
     ptr: *mut rz_core_t
 }
@@ -14,6 +13,30 @@ impl RzCore {
             ptr: unsafe { rz_core_new() }
         }
     }
+
+    pub fn init(&mut self) -> bool {
+        unsafe {
+            rz_core_init(self.ptr)
+        }
+    }
+
+    pub fn rebase(&mut self, addr: u64) -> bool {
+        unsafe {
+            rz_core_bin_rebase(self.ptr, addr)
+        }
+    }
+
+    /*pub fn is_project(&self) -> bool {
+        unsafe {
+            rz_core_is_project(self.ptr)
+        }
+    }*/
+
+    /*pub fn hex_of_assembly(&self, asm: &str) {
+        unsafe {
+            rz_core_hex_of_assembly(self.ptr, );
+        }
+    }*/
 }
 
 impl Drop for RzCore {
@@ -31,9 +54,12 @@ mod tests {
     #[test]
     fn it_works() {
         println!("Creating rizin core");
-        let core = RzCore::new();
 
-        println!("Dropping rizin core");
+        let mut core = RzCore::new();
+
+        core.rebase(0);
+        core.rebase(0x40000);
+
         drop(core);
     }
 }
