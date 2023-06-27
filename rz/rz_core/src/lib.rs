c11e18d80ae85c9669e690ddfec7be668136fbcd
Heather <hbitsoi@tacnetsol.com>
2023-06-27 13:43:23 -0400

    updates

--- rz/rz_core/src/lib.rs
+++ rz/rz_core/src/lib.rs
@@ -79,14 +79,3 @@ impl Drop for RzCore {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
